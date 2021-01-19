e8ed150f2742256a21cc306040c2abc023245f14
Ravi <ravi.dorairaj@icloud.com>
2021-01-18 20:20:18 -0600

    first commit

---
+++ src/main/java/com/ira/IraKub8Application.java
@@ -0,0 +1,13 @@
+package com.ira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IraKub8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IraKub8Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ira/IraKub8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IraKub8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
